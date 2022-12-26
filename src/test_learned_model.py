348e74e4ddf9a14bb4ac81f38197e5aebbe4a59a
Weixuan Zhang <zhangweixuanpeter@gmail.com>
2022-12-26 21:43:36 +0100

    Use GPUs for training

--- src/test_learned_model.py
+++ src/test_learned_model.py
@@ -15,21 +15,21 @@ import matplotlib.pyplot as plt
 # parser = argparse.ArgumentParser()
 # parser.add_argument('model', nargs='?', default="model", help="Path to the model")
 # args = parser.parse_args()
-
-model_path = "/home/weixuan/Documents/Code/blenderproc/model/model_2022_12_26_14_21_21.pt"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model_path = "/home/weixuan/Documents/Code/blenderproc/model/model_2022_12_26_18_14_17.pt"
 
 num_classes = 4
-model = ml.get_object_detector_model(num_classes)
+model = ml.get_object_detector_model(num_classes, device)
 model.load_state_dict(torch.load(model_path))
 model.eval()
 
 #visualization
 # img_path = "/home/weixuan/Documents/Code/blenderproc/data/annotation/images/000019.jpg"
-img_path = "/home/weixuan/Documents/Code/blenderproc/target_images/002.png"
+img_path = "/home/weixuan/Documents/Code/blenderproc/target_images/006.png"
 imgs = Image.open(img_path)
 transform = T.Compose([T.ToTensor()])
 imgs = transform(imgs)
-predictions = model([imgs])
+predictions = model([imgs.to(device)])
 
 #save trained model
 now = datetime.now()
@@ -43,13 +43,13 @@ model_name = "model_" + time  + ".pt"
 # print("saved model to {}".format(save_model_file_path))
 
 class_names = [ '__background__', 'can', 'bottle', 'N/A']
-confidence=0.5
+confidence=0.6
 rect_th=2
 text_size=1
 text_th=1
-pred_class = [class_names[i] for i in list(predictions[0]['labels'].numpy())]
-pred_boxes = [[(int(i[0]), int(i[1])), (int(i[2]), int(i[3]))] for i in list(predictions[0]['boxes'].detach().numpy())]
-pred_score = list(predictions[0]['scores'].detach().numpy())
+pred_class = [class_names[i] for i in list(predictions[0]['labels'].cpu().numpy())]
+pred_boxes = [[(int(i[0]), int(i[1])), (int(i[2]), int(i[3]))] for i in list(predictions[0]['boxes'].detach().cpu().numpy())]
+pred_score = list(predictions[0]['scores'].detach().cpu().numpy())
 pred_t = [pred_score.index(x) for x in pred_score if x>confidence][-1]
 pred_boxes = pred_boxes[:pred_t+1]
 pred_class = pred_class[:pred_t+1]
@@ -58,9 +58,11 @@ import cv2
 img = cv2.imread(img_path)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 # print(len(boxes))
+#do not plot N/A
 for i in range(len(pred_boxes)):
-    cv2.rectangle(img, pred_boxes[i][0], pred_boxes[i][1],color=(0, 255, 0), thickness=rect_th)
-    cv2.putText(img,pred_class[i], pred_boxes[i][0], cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th)
+    if not pred_class[i] == 'N/A':
+        cv2.rectangle(img, pred_boxes[i][0], pred_boxes[i][1],color=(0, 255, 0), thickness=rect_th)
+        cv2.putText(img,pred_class[i], pred_boxes[i][0], cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th)
 plt.figure(figsize=(20,30))
 plt.imshow(img)
 plt.xticks([])
--- test_scripts/test_mask_rcnn_transfer_learning.py
+++ test_scripts/test_mask_rcnn_transfer_learning.py
@@ -14,8 +14,18 @@ from torchvision import datasets, models, transforms
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
+from datetime import date, datetime
+import argparse
+# from sklearn.model_selection import train_test_split
 
-device = "cpu"
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('model', nargs='?', default="model", help="Path to the model")
+args = parser.parse_args()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 class myOwnDataset(torch.utils.data.Dataset):
     def __init__(self, root, annotation, transforms=None):
         self.root = root
@@ -114,7 +124,7 @@ dataset_sizes = len(image_datasets)
 
 #get model
 num_classes = 4
-def get_object_detector_model(num_classes):
+def get_object_detector_model(num_classes, device):
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
     # get the number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
@@ -126,7 +136,7 @@ def get_object_detector_model(num_classes):
     # hidden_layer = 256
     # # and replace the mask predictor with a new one
     # model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask, hidden_layer, num_classes)
-    return model
+    return model.to(device)
 
 #set up training
 import time
@@ -175,7 +185,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     return model
 
 
-model = get_object_detector_model(num_classes)
+model = get_object_detector_model(num_classes, device)
 
 from util_funcs.engine import train_one_epoch, evaluate
 
@@ -186,64 +196,61 @@ criterion = nn.CrossEntropyLoss()
 optimizer_conv = optim.SGD(model.roi_heads.box_predictor.parameters(), lr=0.001, momentum=0.9)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
 
-num_epochs = 5
+num_epochs = 15
 for epoch in range(num_epochs):
     train_one_epoch(model, optimizer_conv, data_loader, device, epoch, print_freq = 1)
 
 # model_conv = train_model(model, criterion, optimizer_conv,
 #                          exp_lr_scheduler, num_epochs=5)
 
-# test object detector and  inference
-images, targets = next(iter(data_loader))
-images = list(image for image in images)
-targets = [{k: v for k, v in t.items()} for t in targets]
-output = model(images,targets)   # Returns losses and detections
 
-model.eval()
+#save trained model
+now = datetime.now()
+today = date.today()
+today_str = today.strftime("%Y_%m_%d")
+current_time = now.strftime("%H_%M_%S")
+time = today_str + "_" + current_time
+model_name = "model_" + time  + ".pt"
+save_model_file_path = "/home/weixuan/Documents/Code/blenderproc/model/" + model_name
+torch.save(model.state_dict(), save_model_file_path)
+print("saved model to {}".format(save_model_file_path))
+print("Training done!")
 
-img_path = "/home/weixuan/Documents/Code/blenderproc/data/annotation/images/000017.jpg"
-imgs = Image.open(img_path)
-transform = T.Compose([T.ToTensor()])
-imgs = transform(imgs)
-predictions = model([imgs])
-
-# plotting
-# class_names = [
-#     '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-#     'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
-#     'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-#     'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
-#     'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-#     'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-#     'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
-#     'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-#     'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
-#     'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-#     'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
-#     'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
-# ]
-
-class_names = [ '__background__', 'can', 'bottle', 'N/A']
-confidence=0.2
-rect_th=2
-text_size=1
-text_th=1
-pred_class = [class_names[i] for i in list(predictions[0]['labels'].numpy())]
-pred_boxes = [[(int(i[0]), int(i[1])), (int(i[2]), int(i[3]))] for i in list(predictions[0]['boxes'].detach().numpy())]
-pred_score = list(predictions[0]['scores'].detach().numpy())
-pred_t = [pred_score.index(x) for x in pred_score if x>confidence][-1]
-pred_boxes = pred_boxes[:pred_t+1]
-pred_class = pred_class[:pred_t+1]
-
-import cv2
-img = cv2.imread(img_path)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-# print(len(boxes))
-for i in range(len(pred_boxes)):
-    cv2.rectangle(img, pred_boxes[i][0], pred_boxes[i][1],color=(0, 255, 0), thickness=rect_th)
-    cv2.putText(img,pred_class[i], pred_boxes[i][0], cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th)
-plt.figure(figsize=(20,30))
-plt.imshow(img)
-plt.xticks([])
-plt.yticks([])
-plt.show()
+# # test object detector and  inference
+# images, targets = next(iter(data_loader))
+# images = list(image for image in images)
+# targets = [{k: v for k, v in t.items()} for t in targets]
+# output = model(images,targets)   # Returns losses and detections
+#
+# model.eval()
+#
+# img_path = "/home/weixuan/Documents/Code/blenderproc/data/annotation/images/000017.jpg"
+# imgs = Image.open(img_path)
+# transform = T.Compose([T.ToTensor()])
+# imgs = transform(imgs)
+# predictions = model([imgs])
+#
+# class_names = [ '__background__', 'can', 'bottle', 'N/A']
+# confidence=0.5
+# rect_th=2
+# text_size=1
+# text_th=1
+# pred_class = [class_names[i] for i in list(predictions[0]['labels'].numpy())]
+# pred_boxes = [[(int(i[0]), int(i[1])), (int(i[2]), int(i[3]))] for i in list(predictions[0]['boxes'].detach().numpy())]
+# pred_score = list(predictions[0]['scores'].detach().numpy())
+# pred_t = [pred_score.index(x) for x in pred_score if x>confidence][-1]
+# pred_boxes = pred_boxes[:pred_t+1]
+# pred_class = pred_class[:pred_t+1]
+#
+# import cv2
+# img = cv2.imread(img_path)
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# # print(len(boxes))
+# for i in range(len(pred_boxes)):
+#     cv2.rectangle(img, pred_boxes[i][0], pred_boxes[i][1],color=(0, 255, 0), thickness=rect_th)
+#     cv2.putText(img,pred_class[i], pred_boxes[i][0], cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th)
+# plt.figure(figsize=(20,30))
+# plt.imshow(img)
+# plt.xticks([])
+# plt.yticks([])
+# plt.show()
